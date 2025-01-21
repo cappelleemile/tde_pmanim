c6f6ee24c42c0843588b87da72573f2dce5183be
TDE Mods <153952211+tdemods@users.noreply.github.com>
2025-01-21 21:00:49 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'TheDevEmile'
-description 'Pauze menu telefoon animatie'
+author 'TDE Mods'
+description 'Pause Menu Phone Animation'
 version '1.0.0'
 
 client_script "client.lua"
